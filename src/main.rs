31887e8a56f46fee0eb4c69d8b498df710cea413
Leonardo Uchôa Pedreira <leonardouchoa.pedreira@gmail.com>
2022-09-25 18:09:46 -0300

    circle implementation

--- src/main.rs
+++ src/main.rs
@@ -14,5 +14,32 @@
 //     TriangleIsosceles,
 // }
 
+
+struct Circle {
+    radius: f64,
+}
+
+impl Circle {
+    fn area(&self) -> f64 {
+        let pi = 3.14159;
+        // println!("This Circle area is {}", self.radius * pi * pi)
+        self.radius * pi * pi
+    }
+    fn circumference(&self) -> f64 {
+        let pi = 3.14159;
+        // println!("This Circle area is {}", 2.0 * pi * self.radius)
+        2.0 * pi * self.radius
+    }
+}
+
 fn main() {
+    // ------------ circulo ------------
+    let circulo: Circle = Circle { radius: 100.0 };
+    println!("The circle radius is {}", circulo.radius);
+    println!("The circle area is {:?}", circulo.area().floor());
+    println!(
+        "The circle circumference is {:?}",
+        circulo.circumference().floor()
+    );
+
 }
