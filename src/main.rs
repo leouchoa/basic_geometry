255e0df6bf630b47d36db6c0c0d196e9f400b648
Leonardo Uchôa Pedreira <leonardouchoa.pedreira@gmail.com>
2022-09-25 18:09:10 -0300

    basic files

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+// Circle
+// Triangle
+// Rectangle
+//
+// enum GeometricFigure {
+//     Circle,
+//     Triangle,
+//     Rectangle,
+// }
+// #[derive(Debug)]
+// enum Triangle {
+//     TriangleEquilateral,
+//     TriangleScalene,
+//     TriangleIsosceles,
+// }
+
+fn main() {
+}
